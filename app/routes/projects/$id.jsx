813caeea9ffc3b98d0b614d6486adec28e33d351
Cernan Bernardo <cernanb@gmail.com>
2021-05-16 16:28:26 -1000

    Fix pending form submit ui

--- app/routes/projects/$id.jsx
+++ app/routes/projects/$id.jsx
@@ -1,5 +1,11 @@
-import React from "react";
-import { useRouteData } from "remix";
+import React, { useRef, useEffect } from "react";
+import {
+  useRouteData,
+  Form,
+  redirect,
+  usePendingFormSubmit,
+  useSubmit,
+} from "remix";
 import db from "../../../db";
 
 export function headers({ loaderHeaders, parentHeaders }) {
@@ -8,11 +14,41 @@ export function headers({ loaderHeaders, parentHeaders }) {
   };
 }
 
+export let action = async ({ request, params }) => {
+  let body = new URLSearchParams(await request.text());
+  switch (request.method.toLowerCase()) {
+    case "post": {
+      let content = body.get("content");
+      await db.task.create({
+        data: {
+          content,
+          projectId: +params.id,
+          complete: false,
+        },
+      });
+      break;
+    }
+    case "put": {
+      console.log(body.get("complete"));
+      await db.task.update({
+        where: {
+          id: +body.get("id"),
+        },
+        data: {
+          complete: JSON.parse(body.get("complete")),
+        },
+      });
+    }
+  }
+  return redirect(`/projects/${params.id}`);
+};
+
 export async function loader({ params }) {
   const project = await db.project.findUnique({
     where: {
       id: +params.id,
     },
+    include: { tasks: true },
   });
   let res = new Response(JSON.stringify(project), {
     headers: { "Content-Type": "application/json" },
@@ -23,6 +59,15 @@ export async function loader({ params }) {
 
 export default function ProjectShow() {
   const project = useRouteData();
+  let pendingFormSubmit = usePendingFormSubmit();
+  const submit = useSubmit();
+  const ref = useRef(null);
+  useEffect(() => {
+    if (pendingFormSubmit) {
+      console.log("submitting");
+      if (ref.current) ref.current.value = "";
+    }
+  }, [pendingFormSubmit]);
   return (
     <div>
       <div className="md:flex md:items-center md:justify-between">
@@ -36,14 +81,113 @@ export default function ProjectShow() {
             type="button"
             className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Delete
+            Deletes
           </button>
           <button
             type="button"
             className="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
           >
-            Complete
+            Completes
           </button>
+        </div>
+      </div>
+      <div className="space-y-6 sm:space-y-5 divide-y divide-gray-200">
+        <div className="pt-6 sm:pt-5">
+          <div role="group" aria-labelledby="label-email">
+            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-baseline">
+              {project.tasks.map((task) => (
+                <div key={task.id} className="mt-4 sm:mt-0 sm:col-span-2">
+                  <div className="max-w-lg space-y-4">
+                    <div className="relative flex items-start">
+                      <div className="flex items-center h-5">
+                        <input
+                          onChange={(e) => {
+                            submit(
+                              {
+                                complete: String(e.target.checked),
+                                id: String(task.id),
+                              },
+                              {
+                                method: "put",
+                                replace: true,
+                              }
+                            );
+                          }}
+                          id="complete"
+                          checked={task.complete}
+                          name="complete"
+                          type="checkbox"
+                          className="focus:ring-green-500 h-4 w-4 text-green-600 border-gray-300 rounded"
+                        />
+                      </div>
+                      <div className="ml-3 text-sm">
+                        <label
+                          htmlFor="comments"
+                          className="font-medium text-gray-700"
+                        >
+                          {task.content}
+                        </label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+              {pendingFormSubmit &&
+                pendingFormSubmit.method.toLowerCase() === "post" && (
+                  <div className="mt-4 sm:mt-0 sm:col-span-2">
+                    <div className="max-w-lg space-y-4">
+                      <div className="relative flex items-start">
+                        <div className="flex items-center h-5">
+                          <input
+                            disabled
+                            id="comments"
+                            name="comments"
+                            type="checkbox"
+                            className="focus:ring-green-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                          />
+                        </div>
+                        <div className="ml-3 text-sm">
+                          <label
+                            htmlFor="comments"
+                            className="font-medium text-gray-400"
+                          >
+                            {pendingFormSubmit.data.get("content")}
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                )}
+              <div className="mt-4 sm:mt-0 sm:col-span-2">
+                <div className="max-w-lg space-y-4">
+                  <div className="relative flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="comments"
+                        name="comments"
+                        type="checkbox"
+                        disabled
+                        className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                      />
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <Form method="post" replace>
+                        <input
+                          ref={ref}
+                          type="text"
+                          name="content"
+                          id="content"
+                          className="shadow-sm focus:ring-gray-500 focus:border-gray-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                          placeholder="New Task..."
+                          aria-describedby="email-description"
+                        />
+                      </Form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
